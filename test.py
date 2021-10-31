31785bd8b58d2ad0687b1973a93133d27041824b
coderLCJ <2907805535@qq.com>
2021-10-31 17:26:19 +0800

    update

--- test.py
+++ test.py
@@ -1,27 +1,12 @@
-def Parse():
-    path = "oxforddata.txt" #文件路径
+# -*- coding: utf-8 -*-
+from pandas import Series,DataFrame
+import numpy as np
+import  pandas as pd
 
-    #将文件内容解析成一个多级字典，并返回
-    #   请在此添加实现代码   #
-	# ********** Begin *********#
-    file = open(path, 'r')
-    for i in range(7):
-        file.readline()
-    dat = {}
-    name = ['tmax', 'tmin', 'af', 'rain', 'sun']
-    for line in file.readlines():
-        line = line.split()
-        if int(line[0]) not in dat:
-            dat[int(line[0])] = {}
-        if int(line[1]) not in dat[int(line[0])]:
-            dat[int(line[0])][int(line[1])] = {}
-        for j in range(2, 7):
-            if line[j] == '---':
-                dat[int(line[0])][int(line[1])][name[j - 2]] = None
-            else:
-                line[j] = line[j].replace('*', '')
-                dat[int(line[0])][int(line[1])][name[j - 2]] = float(line[j])
 
-    return dat
-	# ********** End **********#
-Parse()
+df1 = DataFrame(np.arange(12.).reshape((3, 4)), columns=list('abcd'))
+df2 = DataFrame(np.arange(20.).reshape((4, 5)), columns=list('abcde'))
+df3 = df1.add(df2, fill_value=4)
+
+
+print(df3)
