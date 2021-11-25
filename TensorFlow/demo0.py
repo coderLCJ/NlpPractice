a544a8b2bb5a7966a3b1ba1ecab8ace50dfd2b23
coderLCJ <2907805535@qq.com>
2021-11-25 23:04:25 +0800

    update

---
+++ TensorFlow/demo0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-#
+
+# ---------------------------------------------
+# Name:         demo0
+# Description:  
+# Author:       Laity
+# Date:         2021/11/23
+# ---------------------------------------------
+import tensorflow as tf
+
+t1 = tf.zeros([3, 3])
+print(t1)
--- test2.py
+++ test2.py
@@ -1,9 +1,1 @@
-# 最小二乘法试验
-import numpy as np
-from scipy.optimize import leastsq
-import matplotlib.pyplot as plt
-import pandas as pd
-df2 = pd.DataFrame({'c1': [1, 0, 7],
-                    'c2': [0, 0, 0],
-                    'c3': [3, 0, 9]})
-print(df2)
+print('aaaassddf'.strip('af'))
